04e6a5fff9c53853032ed972c8f2de744bd6efd4
Raymond Shang <shangyuan5000@gmail.com>
2023-11-15 10:32:09 -0700

    compress

--- compress.py
+++ compress.py
@@ -1,2 +1,34 @@
+from typing import List
 class Solution:
     def compress(self, chars: List[str]) -> int:
+        ch0=chars[0]
+        newlen=0
+        i=1
+        for ch in chars[1:]:
+            if ch == ch0:
+                i=i+1
+            else:
+                chars[newlen]=ch0
+                newlen = newlen + 1
+                if i>1:
+                    i_str=str(int(i))
+                    for j in i_str:
+                        chars[newlen]=j
+                        newlen = newlen +1
+                ch0=ch
+                i=1
+        
+        chars[newlen]=ch0
+        newlen = newlen + 1
+        if i>1:
+            i_str=str(int(i))
+            for j in i_str:
+                chars[newlen]=j
+                newlen = newlen +1
+        return newlen
+    
+    
+chars = ["a","a","b","b","c","c","c"]
+
+test = Solution()
+print(test.compress(chars))
