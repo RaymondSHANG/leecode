b6384022057ff9d1f79d07b04eb07e9c63354206
Raymond Shang <shangyuan5000@gmail.com>
2023-11-16 10:42:14 -0700

    maxArea

--- maxArea.py
+++ maxArea.py
@@ -1,9 +1,21 @@
 from typing import List
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        pass
-
-
+        l=0
+        r=len(height)-1
+        max_current = 0 
+        
+        while l<r:
+            if height[l]<height[r]:
+                current_min=height[l]
+                l=l+1
+            else:
+                current_min = height[r]
+                r=r-1
+            area = (r-l+1)*current_min
+            if area > max_current:
+                max_current=area
+        return max_current
 height = [1,8,6,2,5,4,8,3,7]
 s = Solution()
 print(s.maxArea(height))
