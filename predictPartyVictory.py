65d5dba90985115e17923a05cf1a9ce13a1eef0c
Raymond Shang <shangyuan5000@gmail.com>
2023-11-27 13:29:39 -0700

    another solution

--- predictPartyVictory.py
+++ predictPartyVictory.py
@@ -14,3 +14,42 @@ class Solution:
             else:
                 d_pos.append(N+d_temp)
         return 'Radiant' if r_pos else 'Dire'
+
+    def sol2(self, senate: str) -> str:
+        num_r = 0
+        num_d = 0
+
+        # Count the number of D and Rs
+        for mem in senate:
+            if mem == "R":
+                num_r += 1
+            else:
+                num_d += 1
+        ban_r = 0
+        ban_d = 0
+        senate = list(senate)
+        floating_d_bans = 0
+        floating_r_bans = 0
+        # Loop until all D's are banned or all Rs are banned
+        while ban_d != num_d and ban_r != num_r:
+            for i, mem in enumerate(senate):
+
+                # Member is R
+                if mem == 'R':
+                    # There is a ban on R that isn't enforced yet
+                    if floating_r_bans > 0:
+                        floating_r_bans -= 1
+                        senate[i] = 'X'
+                        ban_r += 1
+                    else:
+                        floating_d_bans += 1
+                if mem == 'D':
+                    if floating_d_bans > 0:
+                        floating_d_bans -= 1
+                        senate[i] = 'X'
+                        ban_d += 1
+                    else:
+                        floating_r_bans += 1
+        if ban_r == num_r:
+            return "Dire"
+        return "Radiant"
